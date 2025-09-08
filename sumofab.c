54b44d8ab1b3b8daeb3b6fe00bc9ea08f5863bed
vikas <vikasjakkula08@gmail.com>
2025-09-08 21:33:47 +0530

    update 1.2

---
+++ sumofab.c
@@ -0,0 +1,19 @@
+// sum of a and b
+
+#include <stdio.h>
+int main() {
+    
+    int a,b;
+    
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+    
+    int sum;
+    sum= a+ b;
+    
+    printf("sum:%d ", sum);
+    
+    return 0;
+}
