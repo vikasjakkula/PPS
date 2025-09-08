be65f239d830687e55f03601c27f00470df6353c
vikas <vikasjakkula08@gmail.com>
2025-09-08 19:34:31 +0530

    updated

--- Max.c
+++ Max.c
@@ -2,24 +2,57 @@
 
 #include <stdio.h>
 int main() {
-    int n, max = 0;
-    printf("Enter a number: ");
+    int n;
+    printf("Enter number of elements: ");
     scanf("%d", &n);
+
     int arr[n];
+    printf("Enter %d numbers:\n", n);
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+
+    int max = arr[0];  // assume first element is max
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Maximum = %d\n", max);
+    return 0;
 }
 
 // Second Max of set of n numbers which are in array
 
 #include <stdio.h>
+
 int main() {
-    int n, max = 0;
-    printf("Enter a number: ");
+    int n;
+    printf("Enter number of elements: ");
     scanf("%d", &n);
+
     int arr[n];
-    for(int i = 0; i < n; i++) {
+    printf("Enter %d numbers:\n", n);
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+
+    int max = arr[0], secondMax = -2147483648; // INT_MIN
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            secondMax = max;
+            max = arr[i];
+        } else if (arr[i] > secondMax && arr[i] != max) {
+            secondMax = arr[i];
+        }
+    }
+
+    if (secondMax == -2147483648)
+        printf("No second maximum (all elements are same).\n");
+    else
+        printf("Second maximum is: %d\n", secondMax);
+
+    return 0;
 }
--- binarycal.c
+++ binarycal.c
@@ -2,12 +2,28 @@
 
 #include <stdio.h>
 int main() {
-    /*
-    8 /2 reminder= 0
-    4 /2 reminder= 0
-    2 /2 reminder= 0
+    int decimal, num, i = 0, binary[32];
 
-    reversing the digit's
-    
-    */
+    printf("Enter a decimal number greater than zero: ");
+    scanf("%d", &decimal);
+
+    if (decimal <= 0) {
+        printf("Please enter a number greater than zero.\n");
+        return 1;
+    }
+
+    num = decimal;
+    while (num > 0) {
+        binary[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+
+    printf("The binary number is: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
+
+    return 0;
 }
