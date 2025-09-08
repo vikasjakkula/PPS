9e706690a5f19e2295ad59d4db6b7c1ab2867702
vikas <vikasjakkula08@gmail.com>
2025-09-08 21:43:10 +0530

    update 1.3

---
+++ loop.c
@@ -0,0 +1,14 @@
+// for loop basic code using c lang
+
+#include <stdio.h>
+
+int main()
+{
+    int i,n= 5;
+    for( i=0; i<n; i++ ) {
+        printf("good morning\n");
+    }
+    printf("Hello World");
+
+    return 0;
+}
