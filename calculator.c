c0341530c94c8d5fd6fe86cc45918dd526f08b1f
vikas <vikasjakkula08@gmail.com>
2025-09-11 19:01:01 +0530

    update4

--- calculator.c
+++ calculator.c
@@ -9,5 +9,12 @@ int main() {
     printf("Difference of %d and %d is %d", a, b, a - b);
     printf("Product of %d and %d is %d", a, b, a * b);
     printf("Quotient of %d and %d is %d", a, b, a / b);
+    printf("Remainder of %d and %d is %d", a, b, a % b);
+    printf("Average of %d and %d is %d", a, b, (a + b) / 2);
+    printf("Square of %d is %d", a, a * a);
+    printf("Square of %d is %d", b, b * b);
+    printf("Cube of %d is %d", a, a * a * a);
+    printf("Cube of %d is %d", b, b * b * b);
+    printf("Power of %d to %d is %d", a, b, (int)pow(a, b));
     return 0;
 }
